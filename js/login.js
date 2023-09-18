5a0baa8f1502f36c65d43587e1e155839fae8281
Hélder Medeiros <helderapps@gmail.com>
2023-09-18 14:14:57 -0300

    projeto eng soft

---
+++ js/login.js
@@ -0,0 +1,46 @@
+ 
+let email = document.getElementById('email');
+let senha = document.getElementById('senha');
+let btnEntrar = document.getElementById('btn-entrar');
+
+btnEntrar.addEventListener('click', () => {
+    let userEmail = email.value;
+    let userSenha = senha.value;
+    if(!userEmail || !userSenha){
+        Swal.fire({ icon: 'error',
+            text: 'Os campos de e-mail e senha são obrigatórios!'
+    });
+    return;
+    }     
+    autenticar(userEmail, userSenha);
+});
+
+function autenticar(email, senha){
+   const urlBase = `http://localhost:3400`;
+
+   fetch(`${urlBase}/login`, {
+    method:'POST',
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({email, senha})
+   })
+   .then(response => response = response.json())
+   .then(response => {
+
+       if(!!response.mensagem){
+        alert(response.mensagem);
+        return;
+
+       }else{        
+        salvarToken(response.token);
+        salvarUsuario(response.usuario);
+        setTimeout(() =>{
+            window.open('controle-cliente.html', '_self');
+        })
+        
+       }
+    });
+}
+
+ 
