11237c54a3e3e49233dd25feb6b57cf6ecb1664f
heldermevi <134394976+heldermevi@users.noreply.github.com>
2023-09-22 14:57:23 -0300

    Update Cliente.js

--- js/model/Cliente.js
+++ js/model/Cliente.js
@@ -1,9 +1,6 @@
-// Classe que representa o nosso cliente
-
 class Cliente {
     constructor(obj) {
         obj = obj  || {};
-
         this.id = obj.id;
         this.nome = obj.nome;
         this.cpfOuCnpj = obj.cpfOuCnpj;
